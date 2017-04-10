0a8fcdecdfab86cd5fe37913cb7006abc7ef8697
april <april52522@163.com>
2017-04-10 18:07:10 +0800

    基本框架搭建2

--- app/src/main/java/com/zz/guojin/outsorcingdemo/BaseListActivity.java
+++ app/src/main/java/com/zz/guojin/outsorcingdemo/BaseListActivity.java
@@ -1,23 +1,28 @@
 package com.zz.guojin.outsorcingdemo;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.zz.guojin.outsorcingdemo.utils.ActivityUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class BaseListActivity extends BaseActivity implements
-        SwipeRefreshLayout.OnRefreshListener{
+        SwipeRefreshLayout.OnRefreshListener {
     protected Context context;
     protected Unbinder unbinder;
-    protected int number = 8;
-    protected int currentIndex = 0;
+
     @BindView(R.id.img_back)
     ImageView imgBack;
     @BindView(R.id.title)
@@ -32,11 +37,16 @@ public class BaseListActivity extends BaseActivity implements
     SwipeRefreshLayout swipe;
     @BindView(R.id.content_base_list)
     LinearLayout contentBaseList;
+    Handler myHandler;
+    protected  int number=10;
+    protected int currentIndex =0;
 
     @Override
     public void init() {
         context = this;
         unbinder = ButterKnife.bind(this);
+        ActivityUtils.add(this);
+        myHandler = new Handler();
 
     }
 
@@ -47,13 +57,31 @@ public class BaseListActivity extends BaseActivity implements
 
     @Override
     public void initView() {
+        recycler.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+        recycler.setItemAnimator(new DefaultItemAnimator());
+        swipe.setColorSchemeColors(getResources().getColor(R.color.blue),getResources().getColor(R.color.colorTheme));
+        swipe.setOnRefreshListener(this);
+        // 这句话是为了，第一次进入页面的时候显示加载进度条
+        swipe.setProgressViewOffset(false, 0, (int) TypedValue
+                .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, getResources()
+                        .getDisplayMetrics()));
+        recycler.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
 
     }
 
     @Override
     public void initData() {
-
-
+    //加载数据
     }
 
     @Override
@@ -69,6 +97,13 @@ public class BaseListActivity extends BaseActivity implements
 
     @Override
     public void onRefresh() {
+        myHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                currentIndex=0;
+                initData();
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/zz/guojin/outsorcingdemo/adapter/MyCommonAdapter.java
@@ -0,0 +1,25 @@
+package com.zz.guojin.outsorcingdemo.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/4/10.
+ */
+
+public class MyCommonAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
